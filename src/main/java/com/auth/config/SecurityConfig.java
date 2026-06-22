41ea863ef2a44047afd7b7139667ef94c68de416
LeidyMaingualDev <vanesamaingual2008@gmail.com>
2026-06-22 02:44:29 -0500

    fix: minor SecurityConfig adjustment

--- src/main/java/com/auth/config/SecurityConfig.java
+++ src/main/java/com/auth/config/SecurityConfig.java
@@ -93,7 +93,7 @@ public class SecurityConfig {
                                 "/actuator/health",
                                 "/actuator/info",
                                 "/auth/internal/**",
-                                "/auth/internal/users/**"
+                                "/auth/internal/users/**",
                                 "/api/users/stats",           // estadisticas 
                                 "/api/users/monthly-growth"
                         ).permitAll()
